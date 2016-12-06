fa1832c63d87841a17555b475ec13c874cb31c5a
chenxj92 <842674122@qq.com>
2016-12-06 11:32:10 +0800

    爱奇艺活动

--- js/iQIYI.js
+++ js/iQIYI.js
@@ -1,21 +1,42 @@
 (function($, window, document) {
+	
+	var tl = new TimelineLite();
+	
 	$(function() {
 		
 		fnInit();
 		
 	});
 	
+	// 滑倒指定位置
+    function fnScrollTop($obj) {
+    	tl.clear();
+    	tl.to($('html,body'), 0.8, {
+    		scrollTop: $obj.offset().top
+    	});
+    }
+    
     function fnInit(){
         var $oContent = $('.content'),
+            $oBtnDown = $oContent.find('.btn-down'),
+            $oExchange = $('.exchange'),
             $oBtnVcode = $oContent.find('.btn-vcode'),
             $oBtnBuy = $oContent.find('.btn-buy'),
+            $oMaskResult = $('#mask-result'),
+			$oBtnClose = $oMaskResult.find(".btn-close"),
+			$oBtnOk =  $oMaskResult.find(".btn-ok"),
+			$aLi = $oMaskResult.find('li'),
+            regCity = false,
             regMobile = true, // 手机号验证
             regVcode = true,  // 验证码验证
-            regExchange = true, // 兑换码验证
             tips = '操作失败', // 提示信息
-            icon = 'dialog2/images/icon/fail.png',
             time = 58;
-            
+        
+        // 滑到指定位置
+        $oBtnDown.on('click', function() {
+        	fnScrollTop($oExchange);
+        });
+        
         // 获取验证码
 	    $oBtnVcode.on("click",function(){
 	    	$oBtnVcode.css('backgroundColor','#c8c8c8').text('59" 后重新发送');
@@ -37,28 +58,50 @@
         	if(regMobile){
         		// 验证码验证
         		if(regVcode){
-        			// 兑换码验证
-        			if(regExchange){
-        				tips = '优惠券将于24小时发送至您的账户';
-        				icon = 'dialog2/images/icon/success.png';
-        			}else{
-        				tips = '兑换码不正确';
-        			}
+        			// 选择领券城市
+		    		$oMaskResult.css("display",'block');
         		}else{
-        			tips = '验证码不正确';
+        			fnPopupShow('验证码不正确');
         		}
         	}else{
-        		tips = '手机号码格式不正确';
+        		fnPopupShow('手机号码格式不正确');
         	}
         	
-        	var dialog = $(document).dialog({
-				type: 'toast',
-				infoIcon: icon,
-				infoText: tips,
-				autoClose: '1500'
-			});
-			
+        });
+        
+        // 关闭城市选择弹窗
+        $oBtnClose.on('click', function() {
+        	$oMaskResult.css("display",'none');
+        });
+        
+        // 选择城市
+        $aLi.on('click', function() {
+        	$(this).addClass('active').siblings().removeClass('active');
+        	regCity = true;
+        });
+        
+        // 确定城市选择
+        $oBtnOk.on('click', function() {
+        	$oMaskResult.css("display",'none');
+        	if(regCity){
+        		var dialog = $(document).dialog({
+					type: 'toast',
+					infoIcon: 'dialog2/images/icon/success.png',
+					infoText: '优惠券将于24小时发送至您的账户',
+					autoClose: '1500'
+				});
+        	}
         });
 
     }
+    
+    function fnPopupShow(_tip){
+    	var dialog = $(document).dialog({
+			type: 'toast',
+			infoIcon: 'dialog2/images/icon/fail.png',
+			infoText: _tip,
+			autoClose: '1500'
+		});
+    }
+    
 })(Zepto, window, document);
