3945e7df0d817d4999c2779c679219497fbd18cc
armdong <armdong@163.com>
2016-12-14 09:40:29 +0800

    kol活动页面优化

--- js/kol.js
+++ js/kol.js
@@ -1,63 +1,94 @@
-$(".rule").click(function(){
-			$(".info").show().addClass("animat");
-			$(".shade").show();
-			$('html,body').animate({scrollTop: '170%'}, 100);
-			$(document).bind('touchmove',function(e) {
-			     var e = e || event;
-			     e.preventDefault();
-			});
-			if(Boolean($(".info").show())){
-				$(".shade").click(function(){
-					$(".info").hide();
-					$(".shade").hide();
-					$(document).unbind("touchmove");
-				})
-			}
-		})
-		$(".info span").click(function(){
-			$(".info").hide().removeClass("animat");
-			$(".shade").hide();
-			$(document).unbind("touchmove");
-		});
-		$(".rule_a").click(function(){
-			$(".info_a").show().addClass("animat");
-			$(".shade").show();
-			$('html,body').animate({scrollTop: '730%'}, 100);
-			$(document).bind('touchmove',function(e) {
-			     var e = e || event;
-			     e.preventDefault();
-			})
-			if(Boolean($(".info_a").show())){
-				$(".shade").click(function(){
-					$(".info_a").hide();
-					$(".shade").hide();
-					$(document).unbind("touchmove");
-				})
-			}
-		})
-		$(".info_a span").click(function(){
-			$(".info_a").hide().removeClass("animat");
-			$(".shade").hide();
-			$(document).unbind("touchmove");
-		});
-		$(".rule_b").click(function(){
-			$(".info_b").show().addClass("animat");
-			$(".shade").show();
-			$('html,body').animate({scrollTop: '1250%'}, 100);
-			$(document).bind('touchmove',function(e) {
-			     var e = e || event;
-			     e.preventDefault();
-			});
-			if(Boolean($(".info_b").show())){
-				$(".shade").click(function(){
-					$(".info_b").hide();
-					$(".shade").hide();
-					$(document).unbind("touchmove");
-				})
-			}
-		})
-		$(".info_b span").click(function(){
-			$(".info_b").hide().removeClass("animat");
-			$(".shade").hide();
-			$(document).unbind("touchmove");
-})
+/**
+ * kol圣诞推广页面
+ * @param  {[type]} $        [jQuery对象]
+ * @param  {[type]} window   [window对象]
+ * @param  {[type]} document [document对象]
+ * @return {[type]}          [null]
+ */
+;(function($, window, document) {
+
+    $(function() {
+
+        // 圣诞套餐+雪孩子
+        $(".rule").click(function() {
+            $(".shade").fadeIn();
+            $(document).bind('touchmove', function(e) {
+                var e = e || event;
+                e.preventDefault();
+            });
+            if (Boolean($(".info").fadeIn())) {
+                $(".shade").click(function() {
+                    $(".info").fadeOut(function() {
+                        $(".shade").fadeOut();
+                    });
+                    $(document).unbind("touchmove");
+                })
+            }
+        })
+        $(".info span").click(function() {
+            $(".info").fadeOut(function() {
+                $(".shade").fadeOut();
+            });
+            $(document).unbind("touchmove");
+        });
+
+        // 圣诞老人 + 普通鲜花
+        $(".rule_a").click(function() {
+
+            $(".shade").fadeIn(function() {
+                $(".info_a").fadeIn();
+            });
+
+            $(document).bind('touchmove', function(e) {
+                var e = e || event;
+                e.preventDefault();
+            })
+            if (Boolean($(".info_a").fadeIn())) {
+                $(".shade").click(function() {
+                    $(".info_a").fadeOut(function() {
+                        $(".shade").fadeOut();
+                    });
+                    $(document).unbind("touchmove");
+                })
+            }
+        })
+        $(".info_a span").click(function() {
+            $(".info_a").fadeOut(function() {
+                $(".shade").fadeOut();
+            });
+
+            $(document).unbind("touchmove");
+        });
+
+        // 鲜花套餐
+        $(".rule_b").click(function() {
+
+            $(".shade").fadeIn(function() {
+                $(".info_b").fadeIn();
+            });
+            $(document).bind('touchmove', function(e) {
+                var e = e || event;
+                e.preventDefault();
+            });
+            if (Boolean($(".info_b").fadeIn())) {
+                $(".shade").click(function() {
+                    $(".info_b").fadeOut(function() {
+                        $(".shade").fadeOut();
+                    });
+
+                    $(document).unbind("touchmove");
+                })
+            }
+        })
+        $(".info_b span").click(function() {
+            $(".info_b").fadeOut(function() {
+                $(".shade").fadeOut();
+            });
+
+            $(document).unbind("touchmove");
+        })
+
+
+    });
+
+})(jQuery, window, document);
